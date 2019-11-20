ba638aa1e4de6cfe302d471f08f0d09e9372190d
Jaiki Pitt <jaiki.pitt@student.tsc.nsw.edu.au>
2019-11-20 23:29:06 +1100

    adding things to api...

--- server/main.py
+++ server/main.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restplus import Resource, Api, fields
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
 
-@api.route('/test')
-class Test(Resource):
-    def get(self):
-        """Returns a test message."""
-        return {'test': 'ok'}
+@api.route('/rating/<region>/<sales>')
+@api.doc(params={'region': 'Region for sales of given video game', 'sales':'Sales for given video game'})
+class Rating(Resource):
+    def get(self, region, sales):
+        """Returns the rating."""
+
+        return {'rating': '60'}
+
+@api.route('/sales/<region>/<rating>')
+@api.doc(params={'region': 'Region for sales of given video game', 'rating':'Rating for given video game'})
+class Sales(Resource):
+    def get(self, region, rating):
+        """Returns the sales."""
+
+        return {'sales': '12345'}
 
 
 if __name__ == '__main__':
