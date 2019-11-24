d44b7d61c0d6fd5e979616ddab324812c9fa5c9b
aarushigera55 <aarushigera55@gmail.com>
2019-11-25 09:39:17 +1100

    created features

---
+++ server/features.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+def generation(x):
+    # collection of features
+    feature_dict = {}
+
+    feature_dict['abs_max_Sales'] = max(np.abs(x['Sales']))
+    feature_dict['abs_min_Sales'] = min(np.abs(x['Sales']))
+    feature_dict['range_Sales'] = feature_dict['abs_max_Sales'] - feature_dict['abs_min_Sales']
+    feature_dict['abs_mean_Sales'] = np.mean(np.abs(x['Sales']))
+    return feature_dict
+
+
+
+def generate_features():
+    file = '/Users/aarushigera/Downloads/DSE/Comp/comp9321/VideoGame_Data.csv'
+
+    video_game_dataframe = pd.read_csv(file)
+    video_game_dataframe = video_game_dataframe[['Sales', 'Critic_Score']]
+
+    # get new generated features
+    feature_collection = generation(video_game_dataframe)
+    print(feature_collection)
+    result = pd.DataFrame(list(feature_collection.items()), columns= ['feature', 'value'])
+    print(result)
+
+generate_features()
