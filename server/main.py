12066cff853b364cd11cff27de857ac1791c2801
Jaiki Pitt <jaiki.pitt@student.tsc.nsw.edu.au>
2019-11-24 23:12:57 +1100

    uncommenting master changes

--- server/main.py
+++ server/main.py
@@ -2,13 +2,13 @@ from flask import Flask, abort, jsonify, request
 from flask_restplus import Resource, Api, fields, reqparse, inputs
 import numpy as np
 import sys, traceback
-# from security import *
+from security import *
 
 import pandas as pd
 
 app = Flask(__name__)
 app.app_context().push()
-# auth_init(app)
+auth_init(app)
 api = Api(app)
 
 vgs_csv_file = '../Video_Games_Sales_as_at_22_Dec_2016.csv'
@@ -28,16 +28,16 @@ def clean_vgs_df():
 
 clean_vgs_df()
 
-# ns_security = api.namespace('security', description='Authorization and API key management')
+ns_security = api.namespace('security', description='Authorization and API key management')
 
-# user_model = api.model('user', {
-#     'username': fields.String(required=True, description='user username'),
-#     'password': fields.String(required=True, description='user password')
-# })
+user_model = api.model('user', {
+    'username': fields.String(required=True, description='user username'),
+    'password': fields.String(required=True, description='user password')
+})
 
-# key_model = api.model('key', {
-#     'key': fields.String(required=True, description='API key')
-# })
+key_model = api.model('key', {
+    'key': fields.String(required=True, description='API key')
+})
 
 video_game_model = api.model('video_game', {
     'Name' : fields.String(required=True, description='Name of video game'),
@@ -58,58 +58,57 @@ video_game_model = api.model('video_game', {
     'Rating' : fields.String(required=False, description='Rating of video game')
 })
 
-# @ns_security.route('/login')
-# class UserLogin(Resource):
-#     """
-#         User Login Resource
-#     """
-#     @api.doc('user login')
-#     @api.expect(user_model, validate=True)
-#     def post(self):
-#         """Returns a bearer token after successful authentication."""
-#         post_data = request.json
-#         return login_user(data=post_data)
+@ns_security.route('/login')
+class UserLogin(Resource):
+    """
+        User Login Resource
+    """
+    @api.doc('user login')
+    @api.expect(user_model, validate=True)
+    def post(self):
+        """Returns a bearer token after successful authentication."""
+        post_data = request.json
+        return login_user(data=post_data)
 
 
-# @ns_security.route('/users')
-# class UserList(Resource):
-#     @api.response(201, 'User successfully created.')
-#     @api.expect(user_model, validate=True)
-#     def post(self):
-#         """Creates a new User """
-#         data = request.json
-#         return save_new_user(data=data)
+@ns_security.route('/users')
+class UserList(Resource):
+    @api.response(201, 'User successfully created.')
+    @api.expect(user_model, validate=True)
+    def post(self):
+        """Creates a new User """
+        data = request.json
+        return save_new_user(data=data)
 
 
-# @ns_security.route('/keys')
-# class KeyList(Resource):
+@ns_security.route('/keys')
+class KeyList(Resource):
 
-#     @token_required
-#     def post(self):
-#         """Create a new api key"""
-#         data, _ = get_logged_in_user(request)
-#         user_id = data.get("data").get("user_id")
-#         key = create_key(user_id)
-#         return {
-#                    "message": "Key successfully created.",
-#                    "data": {"key": key}
-#                }, 201
+    @token_required
+    def post(self):
+        """Create a new api key"""
+        data, _ = get_logged_in_user(request)
+        user_id = data.get("data").get("user_id")
+        key = create_key(user_id)
+        return {
+                   "message": "Key successfully created.",
+                   "data": {"key": key}
+               }, 201
 
-#     @token_required
-#     @api.marshal_list_with(key_model, envelope="data")
-#     def get(self):
-#         """Get all api keys for the logged in user"""
-#         data, _ = get_logged_in_user(request)
-#         user_id = data.get("data").get("user_id")
-#         return get_keys(user_id)
+    @token_required
+    @api.marshal_list_with(key_model, envelope="data")
+    def get(self):
+        """Get all api keys for the logged in user"""
+        data, _ = get_logged_in_user(request)
+        user_id = data.get("data").get("user_id")
+        return get_keys(user_id)
 
-#     @token_required
-#     def delete(self, key):
-#         """Remove a certain api key"""
-#         data, _ = get_logged_in_user(request)
-#         user_id = data.get("data").get("user_id")
-#         return delete_key(user_id, key)
-
+    @token_required
+    def delete(self, key):
+        """Remove a certain api key"""
+        data, _ = get_logged_in_user(request)
+        user_id = data.get("data").get("user_id")
+        return delete_key(user_id, key)
 
 groupByParser = reqparse.RequestParser()
 groupByParser.add_argument('category', required=True, choices=["year", "publisher", "genre", "platform", 'developer'], help="Category to group by")
@@ -243,28 +242,29 @@ class GDPtoSales(Resource):
 
         return packet, 200
 
-@api.route('/rating/<region>/<sales>')
-@api.doc(params={'region': 'Region for sales of given video game', 'sales':'Sales for given video game'})
-class Rating(Resource):
-    def get(self, region, sales):
-        """Returns the rating."""
+#Redundant Now?
+# @api.route('/rating/<region>/<sales>')
+# @api.doc(params={'region': 'Region for sales of given video game', 'sales':'Sales for given video game'})
+# class Rating(Resource):
+#     def get(self, region, sales):
+#         """Returns the rating."""
 
-        # rating = getFromML(region,sales)
-        rating = 60
+#         # rating = getFromML(region,sales)
+#         rating = 60
 
-        return {'rating': rating}
+#         return {'rating': rating}
 
+#Redundant Now?
+# @api.route('/sales/<region>/<rating>')
+# @api.doc(params={'region': 'Region for sales of given video game', 'rating':'Rating for given video game'})
+# class Sales(Resource):
+#     def get(self, region, rating):
+#         """Returns the sales."""
 
-@api.route('/sales/<region>/<rating>')
-@api.doc(params={'region': 'Region for sales of given video game', 'rating':'Rating for given video game'})
-class Sales(Resource):
-    def get(self, region, rating):
-        """Returns the sales."""
+#         # sales = getFromML(region, rating)
+#         sales = 12345
 
-        # sales = getFromML(region, rating)
-        sales = 12345
-
-        return {'sales': sales}
+#         return {'sales': sales}
 
 
 # @api.route('/test')
