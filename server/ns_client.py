2ba677bb5f0f8d7ebd2b675afabd0d24a85dd4cd
Espen Kalhagen <espenkal96@gmail.com>
2019-11-25 11:17:58 +1100

    Added auth to frontend

---
+++ server/ns_client.py
@@ -0,0 +1,11 @@
+from flask import Flask, abort, jsonify, request, send_from_directory
+
+from flask_restplus import Resource, Api, fields, Namespace
+
+api = Namespace('client', description='serves a client website')
+
+@api.route('/')
+class Client(Resource):
+
+    def get(self):
+        return send_from_directory('../client/', 'index.html')
